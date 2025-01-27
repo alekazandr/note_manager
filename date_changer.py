7f6ab04b985f5b912ef19be0df7db99adf4a6740
Александр Попов <alekazandr@mail.ru>
2025-01-27 11:36:34 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+created_date = input('введите дату создания заметки в формате "день-месяц-год", например "10-11-2024": ')
+issue_date = input('введите дату истечения заметки (дедлайн) в формате "день-месяц-год", например "10-12-2024": ')
+temp_issue_date = issue_date[:5]
+temp_created_date = created_date[:5]
+
+print(f"Дата создания заметки, {temp_created_date}")
+print(f"Дата истечения заметки, {temp_issue_date}")
