3a468cebc69104849f2b72f2d67b9395264326c8
alekazandr <alekazandr@mail.ru>
2025-01-27 17:12:49 +0300

    27/01/2025

---
+++ add_list.py
@@ -0,0 +1,6 @@
+heading = input("Введите название первого заголовка: ")
+heading2 = input("Введите название второго заголовка: ")
+heading3 = input("Введите название третьего заголовка: ")
+headlines = [heading, heading2, heading3]
+
+print(headlines)
---
+++ date_changer.py
@@ -0,0 +1,5 @@
+temp_issue_date = issue_date[:5]
+temp_created_date = created_date[:5]
+
+print(f"Дата создания заметки, {temp_created_date}")
+print(f"Дата истечения заметки, {temp_issue_date}")
