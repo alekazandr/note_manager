355715331f557dc978cf6e2688804281a5001ed5
Александр Попов <alekazandr@mail.ru>
2025-01-30 12:43:17 +0300

    Add files via upload

--- date_changer.py
+++ date_changer.py
@@ -1,3 +1,7 @@
+# created_date = 'дата создания заметки в формате "день-месяц-год", например "10-11-2024": '
+# issue_date = 'дата истечения заметки (дедлайн) в формате "день-месяц-год", например "10-12-2024": '
+from add_input import issue_date, created_date
+
 temp_issue_date = issue_date[:5]
 temp_created_date = created_date[:5]
 
---
+++ final.py
@@ -0,0 +1,14 @@
+from add_input import username, title, content, status, created_date, issue_date
+from add_list import headlines
+
+note = [
+    username,
+    title,
+    content,
+    status,
+    created_date,
+    issue_date,
+    headlines
+]
+
+print(note)
